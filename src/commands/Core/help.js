9a3e5789725f4c53b05662ead195b883be4c2a5d
royshibom429-lang <royshibom429@gmail.com>
2026-09-21 17:50:14 +0530

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -101,7 +101,7 @@ export async function createInitialHelpMenu(client) {
             },
             {
                 name: '\u200B',
-                value: `-# ${botName} is [open source](https://youtu.be/1jCZX8s3bJE?si=NPOYx-vxVE1I5vJK)`,
+                value: `-# ${botName} is [nhi milega ](https://discord.gg/eyfDmwDSS7)`,
                 inline: false,
             },
         ],
